9468c6a00ad133ea52a83c254ec8b0bea166328e
ajayjamage3 <ajayjamage3@gmail.com>
2022-09-03 19:45:33 +0530

    Logical operators

---
+++ LogicalOp/Problem1.js
@@ -0,0 +1,11 @@
+let year = 1999;
+let age = 2022 - year;
+if(age >= 13 && age <= 19){
+  console.log("Teenage");
+} 
+else if (age>=20 && age<= 29){
+    console.log("Twenties");
+}
+else{
+    console.log("NA");
+}
---
+++ LogicalOp/Problem3.js
@@ -0,0 +1,7 @@
+let a = "h";
+if(a == "a" || a == "e" || a == "i" || a == "o" || a == "u" || a == "A" || a == "E" || a == "I" || a == "O" || a == "U"){
+  console.log("Vowels");
+}
+else{
+  console.log("Consonant");
+}
